dc0bc2692dd9e2e63f2ea5fd9cea6db83c1a8789
codeblogger-02 <mahendranimsatkar02@gmail.com>
2021-07-15 13:39:58 +0530

    poster component added

--- src/pages/Home.page.jsx
+++ src/pages/Home.page.jsx
@@ -2,6 +2,7 @@ import React from "react";
 
 //components
 import EntertainmentCardSlider from "../Components/Entertainment/Entertainmentcard.component";
+import Premier from "../Components/Premier/Premier.component";
 
 
 const HomePage = () => {
@@ -11,6 +12,7 @@ const HomePage = () => {
         <div className="container mx-auto px-20">
             <h1 className="text-2xl font-bold text-gray-800 my-3">The best of Entertainment</h1>
          <EntertainmentCardSlider />
+         <Premier />
          </div>
         </>
     );
