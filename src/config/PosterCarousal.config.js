3171be7c44b3396d6834bd5d12d16a9f2bb2e7cc
codeblogger-02 <mahendranimsatkar02@gmail.com>
2021-07-17 10:58:21 +0530

    fixed one bug for mobile devices

--- src/config/PosterCarousal.config.js
+++ src/config/PosterCarousal.config.js
@@ -1,39 +1,36 @@
 
 
-    const settings = {
-        infinity: false,
-        autoplay: false,
-        slidesToShow: 5,
-        slidesToScroll:2,
-        InitialSlide: 0,
-        responsive:[  
-            {
-            breakpoints:1024,
-            settings:{
-                slidesToShow: 3,
-                slidesToScroll: 2,
-                infinite:true,
-            },
+  const settings = {
+    infinite: false,
+    speed: 500,
+    slidesToShow: 5,
+    slidesToScroll: 4,
+    initialSlide: 0,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3,
+          infinite: true,
         },
-        {
-          breakpoints: 600,
-          settings:{
-              slidesToShow: 2,
-              slidesToScroll: 1,
-              InitialSlide:1,
-          },
       },
-             {
-          breakpoints: 480,
-          settings:{
-              slidesToShow: 2,
-              slidesToScroll: 1,
-             
-          },
-            },
-      
-      ],
-      };
-
-
-      export default settings;
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2,
+          initialSlide: 2,
+        },
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 1,
+        },
+      },
+    ],
+  };
+  
+  export default settings;
