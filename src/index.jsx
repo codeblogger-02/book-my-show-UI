9b801303c82254f5edced54b5fa46c460efd524b
codeblogger-02 <mahendranimsatkar02@gmail.com>
2021-07-22 13:07:10 +0530

    context added

--- src/index.jsx
+++ src/index.jsx
@@ -4,11 +4,17 @@ import './index.css';
 import App from './App';
 import { BrowserRouter} from "react-router-dom";
 
+ 
+//context provider
+import MovieProvider from './context/movie.context';
+
 
 ReactDOM.render(
   <React.StrictMode>
     <BrowserRouter>
+    <MovieProvider>
     <App />
+    </MovieProvider>
     </BrowserRouter>
   </React.StrictMode>,
   document.getElementById('root')
