fbf0b91a79add33aba54c4feaa5cc29383366fcd
codeblogger-02 <mahendranimsatkar02@gmail.com>
2021-07-23 21:12:46 +0530

    update cast and crew

--- src/pages/Movie.page.jsx
+++ src/pages/Movie.page.jsx
@@ -1,5 +1,8 @@
 import {FaCcVisa , FaCcApplePay} from "react-icons/fa";
-import React, {useContext , useState} from 'react';
+import React, {useContext , useState , useEffect} from 'react';
+import axios from "axios";
+import { useParams } from "react-router-dom";
+import Slider from "react-slick";
 
 
 
@@ -15,9 +18,19 @@ import TempPosters from "../config/TempPosters.config";
 //context
 import { MovieContext } from '../context/movie.context';
 
- const Movie = () => {
 
+ const Movie = () => {
+  const {id} = useParams();
   const {movie} = useContext(MovieContext);
+  const[cast,setCast] = useState([]);
+
+  useEffect(() => {
+    const requestCast = async () => {
+      const getCast = await axios.get(`/movie/${id}/credits`);
+      setCast(getCast.data.cast);
+    };
+    requestCast();
+  },[]);
 
     const settings = {
         infinite: false,
@@ -52,6 +65,39 @@ import { MovieContext } from '../context/movie.context';
         ],
       };
     
+      
+      const settingsCast = {
+        infinite: false,
+        speed: 500,
+        slidesToShow: 6,
+        slidesToScroll: 4,
+        initialSlide: 0,
+        responsive: [
+          {
+            breakpoint: 1024,
+            settings: {
+              slidesToShow: 4,
+              slidesToScroll: 3,
+              infinite: true,
+            },
+          },
+          {
+            breakpoint: 600,
+            settings: {
+              slidesToShow: 5,
+              slidesToScroll: 2,
+              initialSlide: 2,
+            },
+          },
+          {
+            breakpoint: 480,
+            settings: {
+              slidesToShow: 2,
+              slidesToScroll: 1,
+            },
+          },
+        ],
+      };
 
     return (
         <>
@@ -102,11 +148,17 @@ import { MovieContext } from '../context/movie.context';
  
       <h2 className="text-gray-800 font-bold text-2xl mb-4">Cast & Crew</h2>
 
-        <div className="flex flex-wrap gap-4">
-             <Cast image="https://in.bmscdn.com/iedb/artist/images/website/poster/large/ben-affleck-292-12-09-2017-05-12-16.jpg" castName="Ben Affleck" role="Batman"/>
-            <Cast image="https://in.bmscdn.com/iedb/artist/images/website/poster/large/henry-cavill-23964-04-05-2020-04-25-14.jpg" castName="henry cavil" role="Superman"/>
-            <Cast image="https://in.bmscdn.com/iedb/artist/images/website/poster/large/gal-gadot-11088-17-10-2017-11-45-36.jpg" castName="Gal Gadot" role="Wonder Woman"/>
-        </div>
+        
+          <Slider {...settingsCast}>
+          {cast.map((castdata) =>
+          ( 
+          <Cast 
+          image={`https://image.tmdb.org/t/p/original/${castdata.profile_path}`}
+           castName={castdata.original_name} 
+           role={castdata.character}/> ))}
+           </Slider>  
+        
+
           </div>
          </div>
 
